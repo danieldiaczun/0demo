1f4cf7c0f70cf4c9a28ff2c96f6480d9d86ef168
ddiaczun <ddiaczun@boldt.com.ar>
2017-04-19 16:35:22 -0300

    spring.jpa.hibernate.naming.physical-strategy

--- demo-controller/src/main/resources/data.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO demo_schema (name) VALUES ('uno');
-INSERT INTO demo_schema (name) VALUES ('dos');
-INSERT INTO demo_schema (name) VALUES ('tres');
--- demo-controller/src/main/resources/schema.sql
+++
@@ -1,1 +0,0 @@
-CREATE TABLE demo_schema (id int IDENTITY NOT NULL, name varchar(255) NULL, PRIMARY KEY (id));
